10751b4ceff4c8f7cef10cbb40c0275d096b1a50
Abie Marshall <abiemarshall94@gmail.com>
2024-12-08 16:36:57 +0000

    introduce library structure

--- tests/operation_tests.rs
+++ tests/operation_tests.rs
@@ -1,13 +1,13 @@
-use calculator::operations::{add, subtract};
+use super_duper_octo_lamp::libs::calculator::operations as op;
 
 #[test]
 fn test_add() {
-    let e: i32 = add(1, 2);
-    let f = add(1, 2);
+    let e: i32 = op::add(1, 2);
+    let f = op::add(1, 2);
     assert_eq!(e, f);
 }
 #[test]
 fn test_substract() {
-    let e: i32 = subtract(2, 1);
+    let e: i32 = op::subtract(2, 1);
     assert_eq!(e, 1);
 }
