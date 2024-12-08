32d13b82274ea74fe904314ab3abd37593f3400d
Abie Marshall <abiemarshall94@gmail.com>
2024-12-08 16:16:52 +0000

    added explicit main function

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Creates a vector with three elements
+    let numbers: Vec<i32> = vec![1, 2, 3];
+
+    // Creates an empty vector of type i32
+    let empty_vec: Vec<i32> = vec![];
+
+    println!("{:?}", numbers); // Output: [1, 2, 3]
+    println!("{:?}", empty_vec); // Output: []
+}
