9b5243281b1dd94773a35f15269b6914b9f872c3
Abie Marshall <abiemarshall94@gmail.com>
2024-12-10 21:29:12 +0000

    unused iteration of directory strings

--- src/entry.rs
+++ src/entry.rs
@@ -6,6 +6,10 @@ pub fn abinit(fs: Option<&VfsPath>) -> Result<(), Box<dyn std::error::Error>> {
 
     let mut _directories: Vec<VfsPath> = root.walk_dir()?.collect::<VfsResult<Vec<_>>>()?;
     _directories.sort_by_key(|path| path.as_str().to_string());
+    let mut directory_strings: Vec<String> = _directories
+        .iter() // Iterate over the _directories vector
+        .map(|path| path.as_str().to_string()) // Convert each path to a String
+        .collect(); // Collect them into a Vec<String>
 
     Ok(())
 }
