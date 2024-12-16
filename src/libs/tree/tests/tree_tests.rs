ccaf81c31dfa22dc902b9c6c421b993d1475adff
Abie Marshall <abiemarshall94@gmail.com>
2024-12-16 19:57:47 +0000

    test fixture creation with files along diferent directory paths

--- src/libs/tree/tests/tree_tests.rs
+++ src/libs/tree/tests/tree_tests.rs
@@ -1,22 +1,63 @@
 use googletest::prelude::*;
-struct MyFixture {
-    common_data: i32,
+use vfs::{MemoryFS, VfsPath, VfsResult};
+struct TestVisitingFileTree {
+    memfs: VfsPath,
 }
 
-impl ConsumableFixture for MyFixture {
+impl TestVisitingFileTree {
+    // Example method that performs some operation on common_data
+    pub fn create_file(&self, name: &str) -> VfsResult<()> {
+        let filepath = self.memfs.join(name)?;
+        let _parent = filepath.parent();
+        _parent.create_dir_all()?;
+        filepath.create_file()?;
+        Ok(())
+    }
+}
+
+impl ConsumableFixture for TestVisitingFileTree {
     fn set_up() -> googletest::Result<Self> {
-        return Ok(MyFixture { common_data: 42 });
+        return Ok(TestVisitingFileTree {
+            memfs: MemoryFS::new().into(),
+        });
     }
 }
 
 #[gtest]
-fn test1(fixture: MyFixture) {
-    println!("Running test1...");
-    assert_eq!(fixture.common_data, 42);
+fn test_files_can_be_created(fixture: TestVisitingFileTree) -> googletest::Result<()> {
+    // Arrange
+    let file_1 = "python_1.py";
+    let file_2 = "python_2.py";
+
+    fixture.create_file(file_1)?;
+    fixture.create_file(file_2)?;
+
+    // Assert
+    let file_1_exists = fixture.memfs.join(file_1)?.exists()?;
+    let file_2_exists = fixture.memfs.join(file_2)?.exists()?;
+
+    verify_that!(file_1_exists, eq(true))?;
+    verify_that!(file_2_exists, eq(true))
 }
 
 #[gtest]
-fn test2(fixture: MyFixture) {
-    println!("Running test2...");
-    assert_eq!(fixture.common_data + 1, 43);
+fn test_directories_can_be_created(fixture: TestVisitingFileTree) -> googletest::Result<()> {
+    // Arrange
+    let file_1 = "foo/python_1.py";
+    let file_2 = "bar/python_2.py";
+
+    fixture.create_file(file_1)?;
+    fixture.create_file(file_2)?;
+
+    // Assert
+    let foo_dir_exists = fixture.memfs.join("foo")?.exists()?;
+    let bar_dir_exists = fixture.memfs.join("bar")?.exists()?;
+
+    let p1_exists = fixture.memfs.join(file_1)?.exists()?;
+    let p2_exists = fixture.memfs.join(file_2)?.exists()?;
+
+    verify_that!(foo_dir_exists, eq(true))?;
+    verify_that!(bar_dir_exists, eq(true))?;
+    verify_that!(p1_exists, eq(true))?;
+    verify_that!(p2_exists, eq(true))
 }
