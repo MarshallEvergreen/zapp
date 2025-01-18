2a2dc32f18157c7a1203762bd4f7b2507dbfbf6a
Abie Marshall <abiemarshall94@gmail.com>
2025-01-18 11:00:33 +0000

    tweaked walk logic

--- src/entry.rs
+++ src/entry.rs
@@ -1,7 +1,7 @@
 use crate::libs::tree::{errors::TreeError, walk::walk};
 use vfs::VfsPath;
 
-pub fn abinit(fs: Option<VfsPath>) -> Result<(), TreeError> {
+pub fn abinit(fs: Option<&VfsPath>) -> Result<(), TreeError> {
     walk(fs)?;
     Ok(())
 }
--- src/libs/tree/walk.rs
+++ src/libs/tree/walk.rs
@@ -1,36 +1,45 @@
 use std::path::PathBuf;
 
-use vfs::{PhysicalFS, VfsPath, VfsResult};
+use vfs::{PhysicalFS, VfsPath};
 
 use super::{errors::TreeError, factory::layer_factory, interface::IPythonLayer};
+pub fn walk(fs: Option<&VfsPath>) -> Result<(), TreeError> {
+    let root: &VfsPath;
 
-pub fn walk(fs: Option<VfsPath>) -> Result<(), TreeError> {
-    let root: VfsPath;
-    let cwd: PathBuf = std::env::current_dir().map_err(|_| TreeError::FileSystemCreationError)?;
+    // null pointer - only created if no file system is provided
+    let _default_fs: Box<VfsPath>;
 
-    if fs.is_some() {
+    if let Some(provided_fs) = fs {
         tracing::info!("File system provided.");
-        root = fs.unwrap();
+        root = provided_fs;
     } else {
         tracing::warn!("No file system provided, using default.");
+        let cwd: PathBuf =
+            std::env::current_dir().map_err(|_| TreeError::FileSystemCreationError)?;
         tracing::info!(
             "Using current working directory as root: '{}'",
             cwd.display()
         );
-        root = PhysicalFS::new(cwd).into();
+        _default_fs = Box::new(PhysicalFS::new(cwd).into());
+        root = _default_fs.as_ref();
     }
 
-    tracing::info!("Root file system path: {}", root.as_str());
-
-    let mut _filepaths: Vec<VfsPath> = root
+    let mut _python_file_paths: Vec<VfsPath> = root
         .walk_dir()
         .map_err(|_| TreeError::WalkDirectoryError)?
-        .collect::<VfsResult<Vec<_>>>()
-        .map_err(|_| TreeError::WalkDirectoryError)?;
+        .filter_map(|entry| {
+            let entry: VfsPath = entry.ok()?;
+            if entry.is_file().ok()? && entry.extension()? == "py" {
+                Some(entry)
+            } else {
+                None
+            }
+        })
+        .collect();
 
-    _filepaths.sort_by_key(|path| path.as_str().to_string());
+    _python_file_paths.sort_by_key(|path| path.as_str().to_string());
 
-    let mut _python_layers: Vec<Box<dyn IPythonLayer>> = _filepaths
+    let mut _python_layers: Vec<Box<dyn IPythonLayer>> = _python_file_paths
         .iter()
         .map(|path| layer_factory(&path))
         .collect::<Result<Vec<_>, _>>()?;
